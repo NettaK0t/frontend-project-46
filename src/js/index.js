c42aa7b32e7bebbc4f20717191e974d83d8c18a6
NettaK0t <taktakot@gmail.com>
2023-02-20 11:44:35 +0300

    upd index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,36 @@
 import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
+import parser from './parser.js';
 
-const genDiff = (filePath1, filePath2) => {
-  const strFromFile1 = fs.readFileSync(path.resolve(filePath1), 'utf8');
-  const strFromFile2 = fs.readFileSync(path.resolve(filePath2), 'utf8');
-  const objFromFile1 = JSON.parse(strFromFile1);
-  const objFromFile2 = JSON.parse(strFromFile2);
+const genDiff = (filePath1, filPath2, format = 'stylish') => {
+  const filePaths = [filePath1, filPath2];
+  const arrOfObjects = filePaths.map((fPath) => {
+    const formatFile = path.extname(fPath);
+    const strFromFile = fs.readFileSync(path.resolve(fPath), 'utf8');
+    return parser(strFromFile, formatFile);
+  });
+  const [objFromFile1, objFromFile2] = arrOfObjects;
   const file1Keys = Object.keys(objFromFile1);
   const file2Keys = Object.keys(objFromFile2);
   const commonUniqKeys = _.sortBy(_.union(file1Keys, file2Keys));
 
-  const diffStr = commonUniqKeys.reduce((str, key) => {
-    if (!file2Keys.includes(key)) {
-      return `${str}  - ${key}: ${objFromFile1[key]}\n`;
-    }
-    if (!file1Keys.includes(key)) {
-      return `${str}  + ${key}: ${objFromFile2[key]}\n`;
-    }
-    if (objFromFile1[key] !== objFromFile2[key]) {
-      return `${str}  - ${key}: ${objFromFile1[key]}\n  + ${key}: ${objFromFile2[key]}\n`;
-    }
-    return `${str}    ${key}: ${objFromFile1[key]}\n`;
-  }, '');
-  return `{\n${diffStr}}`;
+  if (format === 'stylish') {
+    const diffStr = commonUniqKeys.reduce((str, key) => {
+      if (!file2Keys.includes(key)) {
+        return `${str}  - ${key}: ${objFromFile1[key]}\n`;
+      }
+      if (!file1Keys.includes(key)) {
+        return `${str}  + ${key}: ${objFromFile2[key]}\n`;
+      }
+      if (objFromFile1[key] !== objFromFile2[key]) {
+        return `${str}  - ${key}: ${objFromFile1[key]}\n  + ${key}: ${objFromFile2[key]}\n`;
+      }
+      return `${str}    ${key}: ${objFromFile1[key]}\n`;
+    }, '');
+    return `{\n${diffStr}}`;
+  }
+  return null;
 };
 
 export default genDiff;
