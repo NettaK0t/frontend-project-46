1c38cf358ed0f555b5267082dc474949d601dff4
NettaK0t <taktakot@gmail.com>
2023-02-20 11:24:31 +0300

    correct gendiff with cli.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import genDiff from '../src/js/index.js';
 
-const program = new Command();
+import makeHelp from '../src/js/cli.js';
 
-program
-  .name('gendiff')
-  .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => console.log(genDiff(
-    filepath1,
-    filepath2,
-  )));
-program.parse();
+makeHelp();
